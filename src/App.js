163c095e86a54324016e9d2d30e6714ae70f9e53
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-31 21:40:31 -0500

    second commit with local files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Rating from "./components/Rating";
+import FunctionClick from "./components/FunctionClick";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Rating rating='1' />
+      <Rating rating='2' />
+      <Rating rating='3' />
+      <Rating rating='4' />
+      <Rating rating='5' />
+      <br />
+      <FunctionClick />
     </div>
   );
 }
