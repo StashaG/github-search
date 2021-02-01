54ee1be527c0deb6fa39c8349d67962257c4dfbd
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-31 21:53:48 -0500

    editing App.js with a heading temporarily

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
-import Rating from "./components/Rating";
-import FunctionClick from "./components/FunctionClick";
+
 
 function App() {
   return (
     <div>
-      <Rating rating='1' />
-      <Rating rating='2' />
-      <Rating rating='3' />
-      <Rating rating='4' />
-      <Rating rating='5' />
-      <br />
-      <FunctionClick />
+      <h1>Products</h1>
     </div>
   );
 }
